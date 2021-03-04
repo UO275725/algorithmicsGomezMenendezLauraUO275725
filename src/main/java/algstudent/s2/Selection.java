0f34467e473c2b6da38a89ad98097783bcecda20
usuario <usuario@LAPTOP-7VU8MREE>
2021-03-04 19:46:26 +0100

    Trying to upload again session2

---
+++ src/main/java/algstudent/s2/Selection.java
@@ -0,0 +1,35 @@
+﻿package algstudent.s2;
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the SELECTION */
+public class Selection extends Vector {
+	public Selection(int nElements) {
+		super(nElements);
+	}
+	
+	@Override
+	public void sort() {
+		/*
+		 * lowest change first
+		 */
+		for (int i = 0; i < elements.length - 1; i++) 
+		{
+		    for (int j = i + 1; j < elements.length; j++) 
+		    {
+		        int min = elements[j];
+		        if (min < elements[i]) 
+		        {
+		            int aux = elements[i];
+		            elements[i] = elements[j];
+		            elements[j] = aux;
+		        } 
+		    } 
+		} 
+	}  
+	
+	@Override
+	public String getName() {
+		return "Selection";
+	} 
+} 
