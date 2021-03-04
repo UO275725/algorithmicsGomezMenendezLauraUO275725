433515352d91e32a4e4313cb43c44d214c8749b0
usuario <usuario@LAPTOP-7VU8MREE>
2021-03-04 20:02:55 +0100

    Session2 again

---
+++ src/main/java/algstudent/s2/Insertion.java
@@ -0,0 +1,37 @@
+﻿package algstudent.s2;
+
+
+/* This program can be used to sort n elements with 
+ * a "bad" algorithm (quadratic). 
+ * It is the DIRECT INSERTION */
+public class Insertion extends Vector {	
+	public Insertion(int nElements) {
+		super(nElements);
+	}
+
+	@Override
+	public void sort(){
+		/*
+		 * divide into parts
+		 * take first and insert well
+		 */
+		
+		for(int i=1; i<elements.length; i++) {
+			int p = elements[i];
+			int j = i -1;
+		
+			while(j>=0 && elements[j]>p) {
+				elements[j+1] = elements[j];
+				j--;
+			}
+			elements[j+1] = p;
+		}
+		
+		
+	} 
+	
+	@Override
+	public String getName() {
+		return "Insertion";
+	} 
+} 
---
+++ src/main/java/algstudent/s2/QuicksortCentralElement.java
@@ -0,0 +1,42 @@
+﻿package algstudent.s2;
+
+
+/* This program can be used to sort n elements with 
+ * the best algorithm of this lab. It is the QUICKSORT */
+public class QuicksortCentralElement extends Vector {
+	
+	public QuicksortCentralElement(int nElements) {
+		super(nElements);
+	}
+	
+	private void quickSort(int left, int right) {
+		if(left >=right) return;
+	    int leftI = left;
+	    int rightI = right;
+	    int pivot = elements[left + (right - left)/2]; 
+	    while(leftI<=rightI)
+	    {
+	        while(elements[leftI]  < pivot )leftI++;   
+	        while(elements[rightI] > pivot )rightI--;  
+	        if(leftI <=rightI)
+	        {
+	            interchange(leftI, rightI);
+	            leftI++;
+	            rightI--;
+	        }
+	    }
+	    if(left < rightI)quickSort(left,rightI); 
+	    if(leftI < right)quickSort(leftI,right); 
+		
+	}
+
+	@Override
+	public void sort() {
+		quickSort(0, elements.length-1);		
+	}
+	
+	@Override
+	public String getName() {
+		return "Quicksort - Central element";
+	} 
+} 
